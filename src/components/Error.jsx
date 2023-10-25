e10070e5d95d80714d9e17d72796b0c9f5ee3158
nsallitto <nicosallitto@gmail.com>
2023-10-25 10:37:50 -0300

    primer commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+
+function Error({children}) {
+  return (
+    <div className=" bg-red-800 text-white text-center uppercase rounded-md font-bold p-3 mb-3">
+        {children}
+    </div> 
+  )
+}
+
+export default Error
